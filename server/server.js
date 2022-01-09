879d3a9d17a719bde8688d5b5a411483f057e0d1
Christian Escobar F <christian17zxe@gmail.com>
2022-01-08 22:06:11 -0300

    Add Cors

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ import express from 'express'
 import Database from '../db/connection.js';
 import productRoutes from '../routes/product.js'
 import categoryRoutes from '../routes/category.js'
-
+import cors from 'cors'
 class Server {
     constructor() {
         this.app = express()
+        this.app.use(cors())
         this.port = process.env.PORT || 8080
         this.API = {
             product: '/products',
